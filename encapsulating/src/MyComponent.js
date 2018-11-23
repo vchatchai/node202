e96ed5d465a87216a753c294b3041daf7160269d
vchatchai <ee56054@gmail.com>
2018-11-23 14:18:28 +0700

    add excapsulating

---
+++ encapsulating/src/MyComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './MyComponent.css';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <section>
+        <h1>My Component</h1>
+        <p>Content in my compoent...</p>
+      </section>
+    );
+  }
+}
+
+export default MyComponent;
